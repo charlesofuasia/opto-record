aa2ff285935969c9f2393bdb99f3e605764a1239
AbelAguilarAguirre <agu22008@byui.edu>
2025-10-08 18:01:02 -1000

    upcoming doctors appointments in dashboard.

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import DoctorAppointments from "@/app/components/DoctorAppointments";
+import MakeAppointment from "@/app/components/MakeAppointment";
+import UpcomingAppointments from "@/app/components/UpcomingAppointments";
 import { useAuthStore } from "@/store/authStore";
 
 export default function DashboardPage() {
@@ -8,8 +11,12 @@ export default function DashboardPage() {
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">
-                Welcome, {user?.fname || user?.username}!
+                Welcome, {user?.fname || user?.username} {user?.lname}!
             </h1>
+            <DoctorAppointments
+                currentUserId={user?.id || ""}
+                fetchUrl="/api/appointments"
+            />
         </div>
     );
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,109 +3,123 @@
 import Link from "next/link";
 import { useState } from "react";
 import {
-  LayoutDashboard,
-  Users,
-  CalendarDays,
-  Settings,
-  User,
-  LogOut,
-  Menu,
-  X,
+    LayoutDashboard,
+    Users,
+    CalendarDays,
+    Settings,
+    User,
+    LogOut,
+    Menu,
+    X,
 } from "lucide-react";
 import { useAuthStore } from "@/store/authStore";
 import { UserTypesEnum } from "@/constants/roles.enum";
 
 interface User {
-  id: string;
-  type: string;
+    id: string;
+    type: string;
 }
 
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(true);
-  const { user } = useAuthStore();
+    const [isOpen, setIsOpen] = useState(true);
+    const { user } = useAuthStore();
 
-  // Build nav items based on user type
-  const navItems =
-    user?.type == UserTypesEnum.Admin || user?.type == UserTypesEnum.Physician
-      ? [
-        { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
-        { name: "Patients", href: "/patients", icon: Users },
-        { name: "Appointments", href: "/appointments", icon: CalendarDays },
-        { name: "Settings", href: "/settings", icon: Settings },
-      ]
-      : user?.type === UserTypesEnum.Patient
-        ? [
-          {
-            name: "Profile",
-            href: `/patient-portal/${user?.id}`,
-            icon: LayoutDashboard,
-          },
-          {
-            name: "Appointments",
-            href: `/patient-portal/${user?.id}/appointments`,
-            icon: CalendarDays,
-          },
-          { name: "Settings", href: "/settings", icon: Settings },
-        ]
-        : [{ name: "Settings", href: "/settings", icon: Settings }];
+    // Build nav items based on user type
+    const navItems =
+        user?.type == UserTypesEnum.Admin ||
+        user?.type == UserTypesEnum.Physician
+            ? [
+                  {
+                      name: "Dashboard",
+                      href: "/dashboard",
+                      icon: LayoutDashboard,
+                  },
+                  { name: "Patients", href: "/patients", icon: Users },
+                  {
+                      name: "Appointments",
+                      href: "/appointments",
+                      icon: CalendarDays,
+                  },
+                  { name: "Settings", href: "/settings", icon: Settings },
+              ]
+            : user?.type === UserTypesEnum.Patient
+            ? [
+                  {
+                      name: "Profile",
+                      href: `/patient-portal/${user?.id}`,
+                      icon: LayoutDashboard,
+                  },
+                  {
+                      name: "Appointments",
+                      href: "/appointments",
+                      icon: CalendarDays,
+                  },
+                  { name: "Settings", href: "/settings", icon: Settings },
+              ]
+            : [{ name: "Settings", href: "/settings", icon: Settings }];
 
-  return (
-    <aside
-      className={`${isOpen ? "w-64" : "w-20"
-        } shadow-md h-screen transition-all duration-300 flex flex-col bg-primary`}
-    >
-      {/* Logo / Toggle */}
-      <div className="flex items-center justify-between px-4 py-4 border-b">
-        <span className="text-xl font-bold">
-          {isOpen ? "OptoRecord" : "CM"}
-        </span>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="md:hidden text-gray-600 hover:text-blue-600"
+    return (
+        <aside
+            className={`${
+                isOpen ? "w-64" : "w-20"
+            } shadow-md h-screen transition-all duration-300 flex flex-col bg-primary`}
         >
-          {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
-        </button>
-      </div>
+            {/* Logo / Toggle */}
+            <div className="flex items-center justify-between px-4 py-4 border-b">
+                <span className="text-xl font-bold">
+                    {isOpen ? "OptoRecord" : "CM"}
+                </span>
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="md:hidden text-gray-600 hover:text-blue-600"
+                >
+                    {isOpen ? (
+                        <X className="h-6 w-6" />
+                    ) : (
+                        <Menu className="h-6 w-6" />
+                    )}
+                </button>
+            </div>
 
-      {/* Nav Links */}
-      <nav className="flex-1 px-2 py-6 space-y-4">
-        {navItems.map(({ name, href, icon: Icon }) => (
-          <Link
-            key={name}
-            href={href}
-            className="flex items-center space-x-3 hover:text-blue-600"
-          >
-            <Icon className="h-5 w-5" />
-            {isOpen && <span>{name}</span>}
-          </Link>
-        ))}
-      </nav>
+            {/* Nav Links */}
+            <nav className="flex-1 px-2 py-6 space-y-4">
+                {navItems.map(({ name, href, icon: Icon }) => (
+                    <Link
+                        key={name}
+                        href={href}
+                        className="flex items-center space-x-3 hover:text-blue-600"
+                    >
+                        <Icon className="h-5 w-5" />
+                        {isOpen && <span>{name}</span>}
+                    </Link>
+                ))}
+            </nav>
 
-      {/* Footer */}
-      <div className="border-t px-4 py-4">
-        <button className="cursor-pointer flex items-center space-x-3 hover:text-blue-600 w-full">
-          <User className="h-5 w-5" />
-          {isOpen && <span>Profile</span>}
-        </button>
-        <button
-          className="cursor-pointer flex items-center space-x-3 text-red-600 hover:text-red-700 mt-3 w-full"
-          onClick={async () => {
-            try {
-              await fetch("/api/auth/logout", {
-                method: "POST",
-                credentials: "include",
-              });
-            } catch (e) {
-              console.error(e);
-            } finally {
-              window.location.href = "/";
-            }
-          }}
-        >
-          <LogOut className="h-5 w-5" />
-          {isOpen && <span>Logout</span>}
-        </button>
-      </div>
-    </aside>
-  );
+            {/* Footer */}
+            <div className="border-t px-4 py-4">
+                <button className="cursor-pointer flex items-center space-x-3 hover:text-blue-600 w-full">
+                    <User className="h-5 w-5" />
+                    {isOpen && <span>Profile</span>}
+                </button>
+                <button
+                    className="cursor-pointer flex items-center space-x-3 text-red-600 hover:text-red-700 mt-3 w-full"
+                    onClick={async () => {
+                        try {
+                            await fetch("/api/auth/logout", {
+                                method: "POST",
+                                credentials: "include",
+                            });
+                        } catch (e) {
+                            console.error(e);
+                        } finally {
+                            window.location.href = "/";
+                        }
+                    }}
+                >
+                    <LogOut className="h-5 w-5" />
+                    {isOpen && <span>Logout</span>}
+                </button>
+            </div>
+        </aside>
+    );
 }
